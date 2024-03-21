acc85cdb40ea1122ad796dd3e82d09489997f20c
Lucas <lucas.vergara@live.com>
2024-03-20 23:00:11 -0300

    End of the project

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('numbers project');
+
+const items = [...document.querySelectorAll('.number')];
+//console.log(items)
+
+
+const updateCount = (el) => {
+ const value = parseInt(el.dataset.value);
+ const increment = Math.ceil(value/1000)
+ //const increment = 1;
+ let inicialValue = 0;
+
+ const increaseCount = setInterval(()=>{
+    inicialValue += increment
+
+    if(inicialValue > value){
+        el.textContent = `${value}+`;
+        clearInterval(increaseCount);
+        return;
+    }
+
+    el.textContent = `${inicialValue}+`;
+ }, 1);
+console.log(increaseCount);
+};
+
+items.forEach((item1) => {
+updateCount(item1)
+});
+
